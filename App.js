fd9966b12aacfcee1c9dc14e7065a079c339cdd7
chanhong-ji <jichanhong24@gmail.com>
2022-04-02 21:33:03 +0900

    feat: Set preload

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const assetsToLoad = [require("./assets/instagram_logo.png")];
+    const assetPromises = assetsToLoad.map((asset) => Asset.loadAsync(asset));
+    Promise.all([...fontPromises, ...assetPromises]);
+  };
+  if (loading)
+    return (
+      <AppLoading
+        startAsync={preload}
+        onFinish={onFinish}
+        onError={console.warn}
+      />
+    );
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,12 +29,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
